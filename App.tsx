a098b6639ca6e069e0d196b1b58da11485660e1d
SamvelRaja Sakthivel <sam@SamvelRajas-MacBook-Pro.local>
2023-01-09 11:10:07 +0530

    Initial setup with Web3Auth Login

---
+++ App.tsx
@@ -0,0 +1,176 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {useEffect, useState, type PropsWithChildren} from 'react';
+import {
+  Button,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+import { Linking, Alert } from 'react-native'
+import { InAppBrowser } from 'react-native-inappbrowser-reborn'
+
+const getDeepLink = (path = '') => {
+  const scheme = 'my-scheme'
+  const prefix = Platform.OS == 'android' ? `${scheme}://my-host/` : `${scheme}://`
+  return prefix + path
+}
+
+import { LaunchArguments } from "react-native-launch-arguments";
+interface MyExpectedArgs {
+  LeapNearAddress?: string;
+}
+
+
+const onLogin =  async () => {
+    const deepLink = getDeepLink('leap-near-react-native')
+    const url = `http://localhost:3000?redirect_uri=${deepLink}`
+    try {
+      if (await InAppBrowser.isAvailable()) {
+        InAppBrowser.openAuth(url, deepLink, {
+          // iOS Properties
+          ephemeralWebSession: false,
+          // Android Properties
+          showTitle: false,
+          enableUrlBarHiding: true,
+          enableDefaultShare: false
+        }).then((response) => {
+          if (
+            response.type === 'success' &&
+            response.url
+          ) {
+            Linking.openURL(response.url)
+          }
+        })
+      } else Linking.openURL(url)
+    } catch (error) {
+      Linking.openURL(url)
+    }
+}
+
+
+const Section: React.FC<
+  PropsWithChildren<{
+    title: string;
+  }>
+> = ({children, title}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const [injectedJs, setInjectedJs] = useState(`(function() {
+    setTimeout(() => {
+      // window.onReceiveMessage({ method: 'openWeb3Auth', type: 'async' }); 
+    }, 2000)  
+  })();`);
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const handleURL = (url:any) => {
+    if(url) {
+      console.log(url.url);
+      const address = url.url.split('LeapNearAddress=')[1];
+      address && Alert.alert(address)
+    }
+    Linking.removeAllListeners('url');
+  }
+
+  useEffect(() => {
+    Linking.addEventListener('url', handleURL);
+  })
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Login Via Ramper">
+            <Button title='Create Wallet' onPress={() => { console.log('reached') }} />
+          </Section>
+          <Section title="Login via Web3Auth">
+            <Button title='Create Wallet' onPress={() => { console.log('reached'); onLogin(); }} />
+          </Section>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
